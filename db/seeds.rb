da4a6a925e464d1c39ef14079cd8c4d4ea5c052e
Juliette Guerbette <turtleju@gmail.com>
2019-11-06 15:50:16 +0100

    yelp ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+6.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: "chinese"
+  )
+
+end
